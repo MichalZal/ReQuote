7d502ab6e12e06908c24f8e541759ac20cd5f8e4
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-07-15 12:45:35 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
-import Quotes from "./pages/Quotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFound";
-import Comments from './components/comments/Comments'
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'))
+const QuoteDetail = React.lazy(() => import('./pages/QuoteDetail'))
+const NotFound = React.lazy(() => import('./pages/NotFound'))
+const Comments = React.lazy(() => import('./components/comments/Comments'))
+const Quotes = React.lazy(() => import('./pages/Quotes'))
 
 function App() {
 	return (
 		<Layout>
-			<Routes>
-				<Route path="/" element={<Navigate replace to="/quotes" />} />
-				<Route path="/quotes" element={<Quotes />} />
-				<Route path="/quotes/:quoteId" element={<QuoteDetail />} >
-          <Route
-            path=""
-            element={
-              <div className="centered">
-                <Link className="btn--flat" to={`comments`}>
-                  load Comments
-                </Link>
-              </div>
-            }
-          />
-          <Route path={'comments'} element={<Comments />}/>
-        </Route>
-				<Route path="/new-quote" element={<NewQuote />} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+      <Suspense fallback={<div className="centered"><LoadingSpinner /></div>}>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/quotes" />} />
+          <Route path="/quotes" element={<Quotes />} />
+          <Route path="/quotes/:quoteId" element={<QuoteDetail />} >
+            <Route
+              path=""
+              element={
+                <div className="centered">
+                  <Link className="btn--flat" to={`comments`}>
+                    load Comments
+                  </Link>
+                </div>
+              }
+            />
+            <Route path={'comments'} element={<Comments />}/>
+          </Route>
+          <Route path="/new-quote" element={<NewQuote />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
 		</Layout>
 	);
 }
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -5,12 +5,12 @@ import classes from "./MainNavigation.module.css";
 const MainNavigation = () => {
 	return (
 		<header className={classes.header}>
-			<div className={classes.logo}>React Quotes</div>
+			<div className={classes.logo}>ReQuote</div>
 			<nav className={classes.nav}>
 				<ul>
 					<li>
 						<NavLink className={(navData) => navData.isActive ? classes.active : ''} to="/quotes">
-							ReQuote
+							Quotes
 						</NavLink>
 					</li>
 					<li>
